dc72e6f003150dc2a561730c133ffa5d2b256745
unknown <xu_junkai@foxmail.com>
2019-05-10 14:24:17 +0800

    123

--- roles/superset/files/init.sh
+++ roles/superset/files/init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-old_password=$(cat /root/password.txt | awk -F ":" '{print $2}' )
-new_password=$(</dev/urandom tr -dc '12345!@#$%qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c10)
+old_password=$(cat /credentials/password.txt | awk -F ":" '{print $2}' )
+new_password=$(</dev/urandom tr -dc '12345qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c10)
 
 systemctl restart mysql
 mysqladmin -uroot -p${old_password} password $new_password
 
-echo 'Databases root Password:'$new_password  > /root/password.txt
+echo 'Databases root Password:'$new_password  > /credentials/password.txt
 
 sed -i "s/123456/$new_password/" /data/wwwroot/superset/lib/python3.6/site-packages/superset/config.py
 
