a737f138d149ea227699f580d8c7c12410abb172
Mozlk <xu_junkai@foxmail.com>
2019-06-20 17:04:02 +0800

    123

--- roles/superset/files/init.sh
+++ roles/superset/files/init.sh
@@ -6,7 +6,7 @@ new_password=$(</dev/urandom tr -dc '12345qwertQWERTasdfgASDFGzxcvbZXCVB' | head
 systemctl restart mysql
 mysqladmin -uroot -p${old_password} password $new_password
 
-echo 'Databases root Password:'$new_password  > /credentials/password.txt
+echo -e 'MySQL username:root\nDatabases root Password:'$new_password  > /credentials/password.txt
 
 sed -i "s/123456/$new_password/" /data/wwwroot/superset/lib/python3.6/site-packages/superset/config.py
 
