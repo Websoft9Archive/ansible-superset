c91b296ba5332210a1dee74820473d0f4013c9a1
Mozlk <xu_junkai@foxmail.com>
2019-06-21 15:41:40 +0800

    123

--- roles/superset/files/init.sh
+++ roles/superset/files/init.sh
@@ -10,6 +10,7 @@ echo -e 'MySQL username:root\nMySQL Password:'$new_password  > /credentials/pass
 
 sed -i "s/123456/$new_password/" /data/wwwroot/superset/lib/python3.6/site-packages/superset/config.py
 userdel -rf websoft9      
+rm -rf /var/db/sudo/lectured/websoft9                                          
 sed -i "s/\/root\/init.sh//" /etc/rc.local                                                      
 
 rm -rf /root/init.sh
